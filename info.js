008400c3a4dda651525d21de5f10d51e9a3119ba
worsted <ayee.sh@mysummitps.org>
2016-01-11 20:53:17 +0000

    update

---
+++ info.js
@@ -0,0 +1,24 @@
+var item1 = document.getElementById("item1");
+var item2 = document.getElementById("item2");
+var info1 = document.getElementById("info1");
+var info2 = document.getElementById("info2");
+var item3 = document.getElementById("item3");
+var info3 = document.getElementById("info3");
+var item4 = document.getElementById("item4");
+var info4 = document.getElementById("info4");
+
+item1.addEventListener("click", function(){
+    info1.innerHTML = "League of Legends is my all time favorite game and I still play it to this day. I started to play back in middle school. League of Legends is a MOBA, which is an acronym for multiplayer online battle arena. In a MOBA, there are two teams and you can play as a character on one of them. You can go into respective parts of the map where you will face off against other players. Your main goal is to siege and eventually destroy the enemy base commonly reffered to as the nexus. Throughout the game you can acquire objectives that will give your team more gold and buffs. You and your team will use gold in order to purchase items that will allow you to become stronger. With this newfound power, you will be able to take down the towers that are blocking the way to the enemy nexus. Destroying the enemy nexus will grant you victory and reward you with points, that you can use to purchase virtual items.";
+});
+                       
+item2.addEventListener("click", function(){
+    info2.innerHTML = "Brave Frontier is another all time favorite game available on mobile. I played BF back in the last year of middle school. BF is a mobile RPG, which is an acronym for a role playing game. The main protagonist (you) summons powerful spirits to assist you in your journey. Together with other players, you journey and try to complete all of the tasks placed on you. The game is being constantly updated and you can progress to your heart's content. Ultimately you will strive to achieve everything the game has to offer and build the ultimate team setup to beat any challenge in the game.";
+});
+//
+
+item3.addEventListener("click", function(){
+    info3.innerHTML = "Squad is a team based game. You play on one of two teams that can go up to 70 players. You can be soldiers from the US army, or soldiers from ISIS or the Russian army in some cases. The main objective of the game is to capture points located around the map. You will form mini groups (squads) within your team that will dictate what you will do. You can choose what class you can be and each has their own unique strengths and weaknesses. Such as anti-tank classes and marksman snipers. Squad has given me one of the most enjoyable war multiplayer first person shooters. It is quite easily the most realistic and my favorite.";
+});
+item4.addEventListener("click", function(){
+    info4.innerHTML = "Osu is a music rhythm based game. You can tune into all of your favorite songs from your favorite artists. With harder difficulties of each song you have a new pattern to play. You can also modify the song in order for you to achieve more points. The main goal of the game is to complete songs, achieve a good rank on them, and getting as many points as possible in the game.";
+});
